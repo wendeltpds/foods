84ad2397c8c9d183c327b5d7f6f78412aea02e62
wendel tadeu <wendel.tadell1@gmail.com>
2024-04-30 02:15:41 -0300

    feat: add promo banner

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Header from "./_components/header";
 import Search from "./_components/search";
 import CategoryList from "./_components/category-list";
+import Banner from "./../public/Banner.png"
 
 export default function Home() {
   return (
@@ -12,6 +13,16 @@ export default function Home() {
       </div>
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+
+      <div className=" px-5 pt-6" >        
+        <Image src={Banner} 
+          alt="ate 30% de desconto em pizzas"
+          width={0}
+          height={0}
+          className=" w-full h-auto object-contain"
+          sizes="100vw"
+          quality={100} />
       </div>
     </>
   )
