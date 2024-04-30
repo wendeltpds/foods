57e97f89492783500fa621cad6be6c41f3c1dedb
wendel tadeu <wendel.tadell1@gmail.com>
2024-04-30 01:57:47 -0300

    feat: add category itens and component

---
+++ app/_components/category-item.tsx
@@ -0,0 +1,18 @@
+import { Category } from "@prisma/client";
+import Image from "next/image";
+
+interface CategoryItemProps {
+    category: Category
+}
+
+const CategoryItem = ({category}: CategoryItemProps) => {
+    return ( 
+        <div className=" flex items-center gap-3 py-3 px-4 bg-white shadow-md rounded-full" >
+            <Image src={category.imageUrl} alt={category.name} height={30} width={30}/>
+
+            <span className=" text-sm font-semibold" >{category.name}</span>
+        </div>
+     );
+}
+ 
+export default CategoryItem;
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
 import Image from "next/image";
 import Header from "./_components/header";
+import Search from "./_components/search";
+import CategoryList from "./_components/category-list";
 
 export default function Home() {
   return (
-    <Header />
+    <>
+      <Header />
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
+      <div className="px-5 pt-6">
+        <CategoryList />
+      </div>
+    </>
   )
 }
