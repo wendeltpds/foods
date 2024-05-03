9e96bfe571f9c61e5e21882f5861c698039bc0a7
wendel tadeu <wendel.tadell1@gmail.com>
2024-05-03 02:49:27 -0300

    feat: add search for restaurant page

---
+++ app/restaurants/page.tsx
@@ -0,0 +1,38 @@
+"use client"
+import { Restaurant } from "@prisma/client";
+import { notFound, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { searchForRestaurant } from "./_actions/search";
+import Header from "../_components/header";
+import RestaurantItem from "../_components/RestaurantItem";
+
+const Restaurants = () => {
+    const [restaurants, setRestaurants] = useState<Restaurant[]>([])
+    const searchParams = useSearchParams()
+
+    useEffect(() => {
+        const fetchRestaurants = async () => {
+            const searchFor = searchParams.get("search")
+            if(!searchFor) return
+            const foundRestaurants = await searchForRestaurant(searchFor);
+            setRestaurants(foundRestaurants)
+        }
+        fetchRestaurants()
+    }, [searchParams])
+
+    return (
+        <>   
+        <Header />     
+        <div className=" px-5 py-6">
+            <h2 className="text-lg font-semibold mb-6">Restaurantes Encontrados</h2>
+            <div className=" space-y-4 px-5 gap-6 grid grid-cols-2">
+                {restaurants.map((restaurant) => (
+                    <RestaurantItem  key={restaurant.id} restaurant={restaurant} className=" min-w-full max-w-full"/>
+                ))}
+            </div>
+        </div>
+    </>
+    )
+}
+ 
+export default Restaurants;
